d0563e152ea3dd31b0ad326c5e1f7459b468939d
Breebean <41404232+Breebean@users.noreply.github.com>
2020-02-13 20:32:38 +0900

    Add files via upload

--- class.py
+++ class.py
@@ -1,4 +1,9 @@
 class FourCal:
+
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
     def setdata(self, first, second):
         self.first = first
         self.second = second
@@ -7,18 +12,21 @@ class FourCal:
         result = self.first + self.second
         return result
 
+    def mul(self):
+        result = self.first * self.second
+        return result
+
+    def sub(self):
+        result = self.first - self.second
+        return result
+
+    def div(self):
+        result = self.first / self.second
+        return result
 a = FourCal()
 a.setdata(4, 2)
 
-print(a.first)
-print(a.second)
-
 print(a.add())
-
-b = FourCal()
-b.setdata(3, 7)
-
-print(b.first)
-print(b.second)
-
-print(b.add())
+print(a.mul())
+print(a.sub())
+print(a.div())
